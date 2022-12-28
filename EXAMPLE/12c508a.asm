41939690b3bf00704ac76034f1a30ec115c36d93
Larkin <ab88199594ae@mail.ru>
2022-12-29 02:22:45 +0400

    import examples
    pic16f84 example code.

---
+++ EXAMPLE/12c508a.asm
@@ -0,0 +1,52 @@
+;Toggle the GP0 pin at a rate
+;determined by DELAY.
+
+	list p=12c508a
+	#include <p12c508a.inc>
+
+TEMP1	equ	0x08			;Temp variables
+TEMP2	equ	0x09
+TEMP3	equ	0x0A
+
+	org	0x00			;Effective Reset Vector
+
+	clrf	GPIO			;Clear I/O Port
+	movlw	0x00			;Clear W Reg
+	tris	GPIO			;Set PortB as output
+
+LOOP
+	bsf	GPIO, 0			;Set GP0
+	call	DELAY			;Wait
+
+	bcf	GPIO, 0			;Clear GP0
+	call	DELAY			;Wait
+
+	goto	LOOP			;Repeat
+
+;***************************************
+;*  This routine is a software delay.  *
+;*  Fosc = 1/Tosc; Tcycle = 4 x Tosc   *
+;*  Delay = TEMP1xTEMP2xTEMP3xTcycle   *
+;***************************************
+
+DELAY
+
+	movlw	0xFF 
+	movwf	TEMP1			;TEMP1 = 255
+	movwf	TEMP2			;TEMP2 = 255
+	movlw	0x07 
+	movwf	TEMP3			;TEMP3 = 7
+
+DLOOP
+	decfsz  TEMP1, F
+	goto    DLOOP
+
+	decfsz  TEMP2, F
+	goto    DLOOP
+
+	decfsz  TEMP3, F
+	goto    DLOOP
+
+	retlw	0x00
+
+	end		
