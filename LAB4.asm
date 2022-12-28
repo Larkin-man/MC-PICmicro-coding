512ed3759606db5d4024f7b57142357163b3dd57
Larkin <ab88199594ae@mail.ru>
2022-12-29 01:19:00 +0400

    import projects v6
    multiplication

--- LAB4.asm
+++ LAB4.asm
@@ -1,29 +1,39 @@
 #include<p16f84.inc>
-
-INITB 	EQU B'00000000'
-VARA 	EQU 0Ch
-VARB 	EQU 0Dh
-
-ORG 0
-GOTO BEGIN
-ORG 100h
-
+ __CONFIG  _XT_OSC & _CP_OFF & _PWRTE_ON & _WDT_OFF 
+; ячейки ОЗУ
+SCRATCH         EQU     0Ch
+DIGIT           EQU     0Dh
+DAS             EQU     0Eh
+INITA           EQU     B'00000000'
+INITB           EQU     B'00000000'
+        ORG     0
+        GOTO    BEGIN
+;
+        ORG     100h
 BEGIN
-	BCF 	STATUS, RP0
-	CLRF 	PORTB
-	MOVLW 	INITB
-	BSF 	STATUS, RP0
-	MOVWF 	TRISB
+;Инициализация порта А
+        BCF     STATUS,RP0    ;Выбор банка 0
+        CLRF    PORTA	      ;Очистить регистр CNTRLPORT
+        MOVLW   INITA         ;Загpузить B'00000000' в pегистp W
+        BSF     STATUS,RP0    ;Выбор банка 1
+        MOVWF   TRISA         ;Все разряды порта А установить как выходы
+;Инициализация порта В
+        BCF     STATUS,RP0    ;Выбор банка 0
+        CLRF    PORTB	      ;Очистить регистр DATAPORT
+        MOVLW   INITB         ;Загpузить B'00000000' в pегистp W
+        BSF     STATUS,RP0    ;Выбор банка 1
+        MOVWF   TRISB         ;Все разряды порта В установить как выходы 
+;
+NACH
+        BCF     STATUS,RP0  
 
-	BCF 	STATUS, RP0
-
-	MOVLW	D'3'
-	MOVWF	VARA
+	MOVLW	D'5'
+	MOVWF	DAS
 
 	CLRW
 LOOP
-	ADDLW	D'4'
-	DECFSZ	VARA, 1
+	ADDLW	D'2'
+	DECFSZ	DAS, 1
 	GOTO 	LOOP
 
 	MOVWF	PORTB
