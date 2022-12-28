f08927121170033d4d9f0702b8d168f3b947731e
Larkin <ab88199594ae@mail.ru>
2022-12-29 02:11:28 +0400

    re-creation
    re-create all projects.

--- LAB1.asm
+++ LAB1.asm
@@ -1,41 +1,19 @@
 #include<p16f84.inc>
-
-; ячейки ОЗУ
-SCRATCH         EQU     0Ch
-F1              EQU     0Dh
-DIGIT		EQU	0Fh
-DIG		EQU	0Eh
-INITA           EQU     B'11111111'
-INITB           EQU     B'00000000'
-        ORG     0
-        GOTO    BEGIN
-;
-        ORG     100h
+ ORG 0
+ GOTO BEGIN
+ ORG 100h
 BEGIN
-;Инициализация порта А
 
+ BCF STATUS,RP0
+ CLRF PORTB
+ MOVLW B'00000000'
+ BSF STATUS,RP0
+ MOVWF TRISB
+ BCF STATUS,RP0
 
-        BCF     STATUS,RP0    ;Выбор банка 0
-        CLRF    PORTA	      ;Очистить регистр CNTRLPORT
-        MOVLW   INITA         ;Загpузить B'11111111' в pегистp W
-        BSF     STATUS,RP0    ;Выбор банка 1
-        MOVWF   TRISA         ;Все разряды порта А установить как входы
-;Инициализация порта В
-        BCF     STATUS,RP0    ;Выбор банка 0
-        CLRF    PORTB	      ;Очистить регистр DATAPORT
-        MOVLW   INITB         ;Загpузить B'00000000' в pегистp W
-        BSF     STATUS,RP0    ;Выбор банка 1
-        MOVWF   TRISB         ;Все разряды порта В установить как выходы 
-;
-        BCF     STATUS,RP0    ;Выбор банка 0
-        
-START
-MOVLW B'00000011'
-MOVWF PORTB
-	LOOP
-		ADDLW B'00000001'
-		MOVWF PORTB
-	GOTO LOOP
-
-END   
+LOOP
+ MOVLW H'A7'
+ MOVWF PORTB
+ GOTO LOOP
+ END
 
--- LAB4.asm
+++ LAB4.asm
@@ -1,41 +1,25 @@
 #include<p16f84.inc>
-
-; ячейки ОЗУ
-SCRATCH         EQU     0Ch
-DIGIT           EQU     0Dh
-DIG		EQU	0Eh
-INITA           EQU     B'00000000'
-INITB           EQU     B'00000000'
-        ORG     0
-        GOTO    BEGIN
-;
-        ORG     100h
+ ORG 0
+ GOTO BEGIN
+ ORG 100h
 BEGIN
-;Инициализация порта А
-        BCF     STATUS,RP0    ;Выбор банка 0
-        CLRF    PORTA	      ;Очистить регистр CNTRLPORT
-        MOVLW   INITA         ;Загpузить B'00000000' в pегистp W
-        BSF     STATUS,RP0    ;Выбор банка 1
-        MOVWF   TRISA         ;Все разряды порта А установить как выходы
-;Инициализация порта В
-        BCF     STATUS,RP0    ;Выбор банка 0
-        CLRF    PORTB	      ;Очистить регистр DATAPORT
-        MOVLW   INITB         ;Загpузить B'00000000' в pегистp W
-        BSF     STATUS,RP0    ;Выбор банка 1
-        MOVWF   TRISB         ;Все разряды порта В установить как выходы 
-;
-        BCF     STATUS,RP0    ;Выбор банка 0
 
-MOVLW D'2'
-MOVWF PORTB
-CLRW
-MOVLW D'12'
-MOVWF DIGIT
-DECF DIGIT,1
-METKA
-ADDWF PORTB,1
-DECFSZ DIGIT,1
-GOTO METKA
+ BCF STATUS,RP0
+ CLRF PORTB
+ MOVLW B'00000000'
+ BSF STATUS,RP0
+ MOVWF TRISB
+ BCF STATUS,RP0
 
-END
+ MOVLW 05h
+ MOVWF PORTB
+ MOVLW 0h
+M1
+ ADDLW D'10'
+ DECFSZ PORTB,1
+GOTO M1
 
+LOOP
+ MOVWF PORTB
+ GOTO LOOP
+ END
